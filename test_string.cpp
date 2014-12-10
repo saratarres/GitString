b9d71ee134917a4df2fa960beef277ceb7c732d5
lisa <lisa.mallam@live.fr>
2014-12-10 16:25:43 +0100

    test of clear methods

--- test_string.cpp
+++ test_string.cpp
@@ -40,11 +40,13 @@ int main(int argc, char* argv[])
 {
   printf("Hello World, here we are !\n");
 	String Nouvelle_chaine = String();
-	printf(" %d",Nouvelle_chaine.size());
+	printf(" %d\n",Nouvelle_chaine.size());
 	int res = Nouvelle_chaine.empty(3);
 	printf("%d\n",res);
 	res = Nouvelle_chaine.empty(0);
 	printf("%d\n",res);
+	Nouvelle_chaine.clear();
+	printf(" %d",Nouvelle_chaine.size());
 
   return 0;
 }
