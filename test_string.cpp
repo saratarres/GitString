c0c66d8aa3956ac7000c12949d0e93e821ea2e8e
lisa <lisa.mallam@live.fr>
2014-12-15 11:27:52 +0100

    test of operator = methods IT'S WORKS !!! but i have a segmentation fault for operator +=

--- test_string.cpp
+++ test_string.cpp
@@ -39,7 +39,16 @@
 int main(int argc, char* argv[])
 {
   printf("Hello World, here the geeks are !\n");
-	String Nouvelle_chaine = String();
+	String Nouvelle_chaine ("holachicas");
+	int i ;
+	printf(" %s\n", Nouvelle_chaine.Get_string());
+	String NewString ("holachicos");
+	Nouvelle_chaine  = NewString;
+	printf(" %s\n", Nouvelle_chaine.Get_string());
+	char a = 'c';
+	Nouvelle_chaine += a;
+	printf(" %s\n", Nouvelle_chaine.Get_string());
+
 	/*	printf(" %d",Nouvelle_chaine.size());
 	int res = Nouvelle_chaine.empty(3);
 	printf("%d\n",res);
@@ -47,12 +56,13 @@ int main(int argc, char* argv[])
 	printf("%d\n",res);*/
     
     
-    String NewString = String("holachicos");
-    NewString.resize(4);
+  
+		
+  //NewString.resize(4);
 	//printf("%d\n",NewString.size());
 	//printf("%d\n",NewString.at(4));
 
-	NewString.resize(8);
+	//NewString.resize(8);
 	//printf("%d\n",NewString.size());
 	//printf("%d\n",NewString.at(4));
 
